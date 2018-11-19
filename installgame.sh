99ed9a1207dfab27a6073df42cf1a27d8be8bc0c
panito69 <11649060+panito69@users.noreply.github.com>
2018-11-19 20:55:46 +0100

    Missing space on if

--- installgame.sh
+++ installgame.sh
@@ -36,7 +36,7 @@ printf '#################################
 #################################\n'
 if [[ $server_game == 'csgo' ]]; then
   dpkg --add-architecture i386; sudo apt update; sudo apt install -y mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 libstdc++6:i386
-elif [[ $server_game == 'mc']]; then
+elif [[ $server_game == 'mc' ]]; then
   sudo dpkg --add-architecture i386; sudo apt update; sudo apt install -y mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux default-jre
 fi
 
